c3b84ff0417a4ee4c135473d340d3895c82a47dc
zhivko-doc <45368048+zhivko-doc@users.noreply.github.com>
2019-10-14 13:06:23 +0100

    Update and rename HardDrive.java to TakeInput.java

---
+++ TakeInput.java
@@ -0,0 +1,30 @@
+import java.io.*;
+
+public class TakeInput
+{
+	public static void main(String[] args)
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String l = null;
+		String[] ls = null;
+		int n = 0;
+		int[] array = null;
+		try
+		{
+			l = br.readLine();
+			ls = l.split(" ");
+			n = Integer.parseInt(ls[0]);
+			
+			l = br.readLine();
+			ls = l.split(" ");
+			for (int i = 0; i < ; i++)
+			{
+				
+			}
+
+		} catch (IOException e)
+		{
+		}
+	}
+
+}
